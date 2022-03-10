13b9d23e0e5d289fbf6e7806cb551478cbc1e5d7
Jinay Shah <shahjinay02@gamil.com>
2022-03-10 23:06:54 +0530

    Second Update

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,33 @@ void main() => runApp(myApp());
 class myApp extends MaterialApp {
   Widget? get home => Scaffold(
         appBar: AppBar(
-          title: Text('Hello'),
+          title: Text('jio'),
         ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Scaffold(
+                    body: ListView(
+                  children: [
+                    Container(
+                      child: Image(
+                        image: AssetImage('images/download.png'),
+                      ),
+                    ),
+                    Container(
+                      child: Text('Application 2'),
+                    ),
+                  ],
+                )),
+              ),
+              ListTile(
+                title: Text('Rahi'),
+                leading: Icon(Icons.add_box),
+              )
+            ],
+          ),
+        ),
+        drawerScrimColor: Colors.pink[200],
       );
 }
